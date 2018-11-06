fcffe7a10735f53e32e7a380dcc0a0d7ccea44ea
Rasmus Henriksen <rh@smartweb.dk>
2018-11-06 10:51:31 +0100

    added optional NGROK_AUTH token argument

--- smartweb-entrypoint.sh
+++ smartweb-entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-echo "Starting ngrok with ${WEBSHOP_URL}"
 if [ -z "$WEBSHOP_URL" ]; then
   echo "You must specific the webshop frontend fx sw0001.smartweb.test"
   exit 1
 fi
 
-#exec ngrok http -region=eu -host-header=${WEBSHOP_URL} ${WEBSHOP_URL}:80
-exec ngrok http -region=eu ${WEBSHOP_URL}:80
+if [ -n "$NGROK_AUTH" ]; then
+    echo "Starting ngrok with ${WEBSHOP_URL} and auth token"
+    exec ngrok http -authtoken=${NGROK_AUTH} -region=eu ${WEBSHOP_URL}:80
+else
+    echo "Starting ngrok with ${WEBSHOP_URL}"
+    exec ngrok http -region=eu ${WEBSHOP_URL}:80
+fi
